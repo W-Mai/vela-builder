dc93284ccbbd75f214fd17bef9b13b36790e2b7a
xinbingnan <xinbingnan@xiaomi.com>
2025-10-09 21:07:22 +0800

    add podman missing check
    Signed-off-by: xinbingnan <xinbingnan@xiaomi.com>

--- commands/vela-builder.sh
+++ commands/vela-builder.sh
@@ -3,6 +3,24 @@ container_name="vela-builder-${PWD##*/}"
 ssh_src="$HOME/.ssh"
 ssh_dst="/root/.ssh"
 
+RED=$(tput setaf 1)
+GREEN=$(tput setaf 2)
+YELLOW=$(tput setaf 3)
+RESET=$(tput sgr0)
+
+if ! command -v podman >/dev/null 2>&1; then
+    echo "${RED}Podman is not installed${RESET}" >&2
+    echo "${YELLOW}Please install first：${RESET}" >&2
+    echo "  ${GREEN}# Debian/Ubuntu${RESET}" >&2
+    echo "  sudo apt update && sudo apt install -y podman" >&2
+    echo "  ${GREEN}# Fedora/RHEL/CentOS${RESET}" >&2
+    echo "  sudo dnf install -y podman" >&2
+    echo "  ${GREEN}# Arch Linux${RESET}" >&2
+    echo "  sudo pacman -S --noconfirm podman" >&2
+    echo "  paru -S podman" >&2
+    exit 1
+fi
+
 if [[ $# -eq 0 ]]; then
     podman exec -it "$container_name" bash 2>/dev/null || \
     podman run -it --rm \
