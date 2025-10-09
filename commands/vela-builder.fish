819131a4537243b90b15921978bad7a51a801fa4
xinbingnan <xinbingnan@xiaomi.com>
2025-10-09 21:10:31 +0800

    add podman missing check
    Signed-off-by: xinbingnan <xinbingnan@xiaomi.com>

--- commands/vela-builder.fish
+++ commands/vela-builder.fish
@@ -1,7 +1,26 @@
 function vela-builder
+    set -e RED;    set RED    (set_color red)
+    set -e GREEN;  set GREEN  (set_color green)
+    set -e YELLOW; set YELLOW (set_color yellow)
+    set -e RESET;  set RESET  (set_color normal)
+
     set -l container_name "vela-builder-"(basename (pwd))
     set -l ssh_src $HOME/.ssh
     set -l ssh_dst /root/.ssh
+
+    if ! type -q podman
+        echo $RED"Podman is not installed"$RESET >&2
+        echo $YELLOW"Please install first："$RESET >&2
+        echo "  "$GREEN"# Debian/Ubuntu"$RESET >&2
+        echo "  sudo apt update && sudo apt install -y podman" >&2
+        echo "  "$GREEN"# Fedora/RHEL/CentOS"$RESET >&2
+        echo "  sudo dnf install -y podman" >&2
+        echo "  "$GREEN"# Arch Linux"$RESET >&2
+        echo "  sudo pacman -S --noconfirm podman" >&2
+        echo "  paru -S podman" >&2
+        return -1
+    end
+
 
     if test (count $argv) -eq 0
         podman exec -it $container_name bash 2>/dev/null; or \
